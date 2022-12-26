33de49641ab61ffeb19826e683cefffd9452ee90
Justin Cook <justin@Justins-MacBook-Air.local>
2022-12-26 02:19:42 -0500

    Initial commit

---
+++ src/main/java/com/in28minutes/springboot/studentservices/StudentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.studentservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/springboot/studentservices/StudentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.studentservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
