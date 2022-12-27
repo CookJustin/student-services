b3c028ba993084adefca34a7e09321219e51df6d
Justin Cook <justin@Justins-MacBook-Air.local>
2022-12-26 21:48:07 -0500

    localhost:8080 gives hi

---
+++ src/main/java/com/in28minutes/springboot/studentservices/REST.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.studentservices;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class REST {
+    @GetMapping
+    public static ResponseEntity<String> getHello(){
+        return new ResponseEntity<>("Hi", HttpStatus.ACCEPTED);
+    }
+
+}
--- src/main/java/com/in28minutes/springboot/studentservices/StudentServicesApplication.java
+++ src/main/java/com/in28minutes/springboot/studentservices/StudentServicesApplication.java
@@ -2,6 +2,10 @@ package com.in28minutes.springboot.studentservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class StudentServicesApplication {
@@ -9,5 +13,4 @@ public class StudentServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServicesApplication.class, args);
 	}
-
 }
