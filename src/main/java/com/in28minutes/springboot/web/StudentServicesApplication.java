20d1e56ef8f03fc2e49ea71694aeb2dbb3024cf2
Justin Cook <justin@Justins-Air.attlocal.net>
2023-01-16 17:06:22 -0600

    testing multithreading

--- src/main/java/com/in28minutes/springboot/web/StudentServicesApplication.java
+++ src/main/java/com/in28minutes/springboot/web/StudentServicesApplication.java
@@ -1,13 +1,19 @@
 package com.in28minutes.springboot.web;
 
+import com.in28minutes.springboot.web.multithreading.MultiThreadThing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.in28minutes.springboot.web")
 public class StudentServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServicesApplication.class, args);
+		for(int i = 0; i < 5; i++){
+			MultiThreadThing myThing = new MultiThreadThing(i);
+			Thread myThread = new Thread(myThing);
+			myThread.start();
+			myThread.isAlive(); //stop executing until thread completes
+		}
 	}
 }
